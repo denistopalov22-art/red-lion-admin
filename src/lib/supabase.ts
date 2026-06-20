74ef7f2b5d5ba0626e07a8892a3f10ffb82c6b11
denistopalov22-art <denistopalov22@outlook.com>
2026-06-20 19:40:22 +0100

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,11 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
+const supabaseUrl = 'https://vumvxkqwkaiknilkoyzj.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ1bXZ4a3F3a2Fpa25pbGtveXpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE2ODM4MTMsImV4cCI6MjA5NzI1OTgxM30.XjQRdwmF7CfdrVu_Z23ZScK3F6MwU6wm4agSVp1p9SA';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
     persistSession: true,
     autoRefreshToken: true,
   },
-});
+} );
